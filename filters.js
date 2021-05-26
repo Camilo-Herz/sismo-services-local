6f3768b351c5c697b4ab0e8dc0da192eefe8cdb1
Kevim.Marinio <vimek96@gmail.com>
2021-05-26 03:06:12 -0500

    email

--- filters.js
+++ filters.js
@@ -1,8 +1,30 @@
 const data = require("./dataUser.json");
 const dataFilter = require("./response.json");
 const jwt_decode = require('jwt-decode');
+const moment = require('moment');
+
 
 const OPC = {
+  alert: () => {
+    let alerts = {};
+    const dataProject = dataFilter.nombreProceso.replace(/_/g, " ").split("=>");
+    data.payload.projects.map((element) => {
+      if (element.nombreProceso === dataProject[0]) {
+        element.topics.map((element2) => {
+          if (element2.alert===true){
+            alerts[element2.name] = {
+              valueAlert: element2.valueAlert,
+              units: element2.units,
+              hour:  moment(new Date().getHours()+':'+new Date().getMinutes(),'HH:mm'),
+              env: false,
+              namepro: dataProject[0]
+            } 
+          }
+        });
+      }
+    });
+    return alerts;
+  },
   endpoint: () => {
     let endpoind = "";
     const dataProject = dataFilter.nombreProceso.replace(/_/g, " ").split("=>");
