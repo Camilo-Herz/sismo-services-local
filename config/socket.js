5181176b38d73619371d9b16a5dc49bad24a80d1
Kevim.Marinio <vimek96@gmail.com>
2021-06-30 23:37:47 -0500

    update v1

--- config/socket.js
+++ config/socket.js
@@ -1,12 +1,14 @@
 const io = require("socket.io-client");
-const { clientIdFull } = require("../filters");
+const topic = require('../filters').topics;
+const idProject = require('../filters').idProject();
 const clientId = require('../filters').clientId();
+const clientIdFull = require('../filters').clientIdFull();
 const socket = io("https://sismo-service.herokuapp.com", {
 //const socket = io("http://localhost:3700", {
     reconnection: true,
     autoConnect: true,
     query: {
-        nameRoom: clientId
+        nameRoom: clientId+'@'+idProject
     },
     extraHeaders: {}
 });
@@ -19,11 +21,11 @@ const webSocket = {
            // console.log('datos recibidos: ', res);
         });
     },
-    emitSocket: (req) => {
+    emitSocket: (req,id) => {
         socket.emit("event", {
             userId: "606f1023bfb4bf0c62d543a2",
-            idProject: "780",
-            topic: "Rxdata1",
+            idProject: idProject,
+            topic: id,
             dataTopic: req,
             date: new Date()
         });   
